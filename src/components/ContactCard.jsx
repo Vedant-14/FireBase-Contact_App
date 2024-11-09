44972424d6f1f348cc4232402f2a8865359c5aae
[Vedant Zawar] <[vedantzawar@gmail.com]>
2024-11-10 00:11:56 +0530

    Delete Contact Function Added

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,22 +1,45 @@
 import {HiOutlineUserCircle} from 'react-icons/hi';
 import {MdEdit,MdDeleteForever} from 'react-icons/md';
-const ContactCard = ({id,name,email}) => {
+import {db} from '../config/Firebase';
+import {deleteDoc,doc,addDoc,updateDoc} from 'firebase/firestore';
+import AddAndUpdateContact from './AddAndUpdateContact';
+import { useState } from 'react';
+import useDisclose from '../Hooks/useDisclose';
+import { toast } from 'react-toastify';
+
+// import AddAndUpdateContact from './AddAndUpdateContact';
+
+const ContactCard = ({contact}) => {
+  const {isOpen,toggleModal} = useDisclose();
+
+const deleteContact = async (id)=> {
+  try {
+    toast.success("Contact Deleted Succssfully");
+    await deleteDoc(doc(db,"contacts",id));
+  }
+  catch(error) {
+    console.log(error);
+  }
+}
+const updateContact = ()=> {
+  toggleModal();
+}
   return (
-    
+    <>
       <div
-        key={id}
+        key={contact.id}
         className="bg-yellow flex gap-1 justify-between items-center p-2 rounded-lg"
       >
         <div className="flex items-center gap-1 ">
           <HiOutlineUserCircle className="text-4xl" />
           <div className="">
-            <h2 className="font-medium">{name}</h2>
-            <p className="text-sm">{email}</p>
+            <h2 className="font-medium">{contact.name}</h2>
+            <p className="text-sm">{contact.email}</p>
           </div>
         </div>
         <div className="flex gap-2 cursor-pointer text-3xl">
-          <MdEdit className="" />
-          <MdDeleteForever />
+          <MdEdit className="" onClick={updateContact} />
+          <MdDeleteForever onClick={()=>deleteContact(contact.id)}data-id={contact.id} className='delete-button' />
         </div>
       </div>
     
